1f4e6f2e360551c692d5b46eae0a858b32b705f5
Jesus Capellan <jesus.k.capellan@outlook.com>
2024-11-05 14:02:28 -0500

    Revert unintended changes

--- src/app/user/[userId]/page.tsx
+++ src/app/user/[userId]/page.tsx
@@ -1,5 +1,4 @@
 import { notFound } from "next/navigation";
-import { getUserController } from "~/controllers/user";
 import { db } from "~/db";
 
 export default async function UserPage({
@@ -7,11 +6,15 @@ export default async function UserPage({
 }: {
     params: { userId: string };
 }) {
-    const data = await getUserController(params.userId);
-    if (!data.ok) {
+    if (isNaN(parseInt(params.userId))) {
         notFound();
     }
-    const user = data.data;
+    const user = await db.query.users.findFirst({
+        where: (users, { eq }) => eq(users.userId, parseInt(params.userId)),
+    });
+    if (!user) {
+        notFound();
+    }
     return (
         <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
             <div className="container flex flex-col items-center justify-center gap-8 px-4 py-16">
