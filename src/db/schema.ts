57857360a88842c7c31724aaa3060fa1e8b61e5d
aloomdar <aqanoongo@gmail.com>
2024-11-06 15:46:30 -0500

    created schemas, check if appointmentDate is correct

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,9 +2,11 @@ import { sql } from "drizzle-orm";
 import {
     boolean,
     index,
+    json,
     pgEnum,
     pgTableCreator,
     serial,
+    text,
     timestamp,
     varchar,
 } from "drizzle-orm/pg-core";
@@ -41,3 +43,94 @@ export const users = createTable(
         emailIndex: index("email_idx").on(example.email),
     }),
 );
+
+export const authentication = createTable(
+    "authentication",
+    {
+        authId: serial("auth_id").primaryKey(),
+        userId: serial("user_id").notNull().references(() => users.userId),
+        username: varchar("username", {length:256}).notNull(),
+        passwordHash: varchar("password_hash", {length: 256}),
+        lastLogin: timestamp("last_login").$onUpdate(() => sql`CURRENT_TIMESTAMP`).notNull(),
+        createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+        updatedAt: timestamp("updated_at").$onUpdate(()=> sql`CURRENT_TIMESTAMP`)
+    }
+);
+
+export const licenseVerification = createTable(
+    "license_verification",
+    {
+        verificationId: serial("verification_id").primaryKey(),
+        userId: serial("user_id").notNull().references(() => users.userId),
+        licenseNumber: varchar("license_number", {length: 50}).notNull(),
+        isVerified: boolean("is_verified").default(false),
+        createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+        updatedAt: timestamp("updated_at").$onUpdate(()=> sql`CURRENT_TIMESTAMP`)
+    }
+);
+
+export const healthHabits = createTable(
+    "health_habits",
+    {
+        habitId: serial("habit_id").primaryKey(),
+        userId: serial("user_id").notNull().references(()=> users.userId),
+        date: timestamp("date").default(sql`CURRENT_TIMESTAMP`).notNull(),
+        waterIntake: serial("water_intake").notNull(),
+        sleepHours: serial("sleep_hours").notNull(),
+        mealsEaten: serial("meals_eaten").notNull(),
+        createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+        updatedAt: timestamp("updated_at").$onUpdate(()=> sql`CURRENT_TIMESTAMP`)
+    }
+);
+
+export const surveyEnum = pgEnum("survey_type", ["Initial", "Daily"]);
+export const surveys = createTable(
+    "surveys",
+    {
+        surveyId: serial("survey_id").primaryKey(),
+        userId: serial("user_id").notNull().references(()=>users.userId),
+        surveyDate: timestamp("survey_date").default(sql`CURRENT_TIMESTAMP`).notNull(),
+        surveyType: surveyEnum("survey_type"),
+        surveyData: json("survey_data").notNull(),
+        createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+        updatedAt: timestamp("updated_at").$onUpdate(()=> sql`CURRENT_TIMESTAMP`)
+    }
+);
+
+export const statusEnum = pgEnum("status", ["Pending", "Approved", "Declined"]);
+export const therapistPatient = createTable(
+    "therapist_patient",
+    {
+        relationshipId: serial("relationship_id").primaryKey(),
+        patientId: serial("patient_id").notNull().references(()=>users.userId),
+        therapistId: serial("therapistId").notNull().references(()=>users.userId),
+        status: statusEnum("status").default("Pending"),
+        createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+        updatedAt: timestamp("updated_at").$onUpdate(()=> sql`CURRENT_TIMESTAMP`)
+    }
+);
+
+export const therapistComments = createTable(
+    "therapist_comments",
+    {
+        commentId: serial("comment_id").primaryKey(),
+        patientId: serial("patient_id").notNull().references(()=>users.userId),
+        therapistId: serial("therapistId").notNull().references(()=>users.userId),
+        comment: text("comment").notNull(),
+        createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+        updatedAt: timestamp("updated_at").$onUpdate(()=> sql`CURRENT_TIMESTAMP`)
+    }
+);
+
+export const appointments = createTable(
+    "appointments",
+    {
+        appointmentId: serial("appointment_id").primaryKey(),
+        patientId: serial("patient_id").notNull().references(()=>users.userId),
+        therapistId: serial("therapistId").notNull().references(()=>users.userId),
+        appointmentDate: timestamp("appointment_date").notNull(),
+        status: statusEnum("status").default("Pending"),
+        createdAt: timestamp("created_at").default(sql`CURRENT_TIMESTAMP`).notNull(),
+        updatedAt: timestamp("updated_at").$onUpdate(()=> sql`CURRENT_TIMESTAMP`)
+    }
+)
