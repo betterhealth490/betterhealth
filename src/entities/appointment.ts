a420de583a25d7b0032d3288402dbf42eda5f48b
aloomdar <aqanooongo@gmail.com>
2024-11-12 18:43:18 -0500

    Fix health habit and appointment entities

--- src/entities/appointment.ts
+++ src/entities/appointment.ts
@@ -1,14 +1,51 @@
-export interface GetAppointmentInput{
-    userId: string,
-    date: Date
+export interface GetAppointmentInput {
+  date: Date;
+  notes: string;
 }
 
-export interface GetAppointmentResult{
-    appointmentDate: Date,
-    appointmentId: number,
-    status: "Pending" | "Confirmed" | "Cancelled",
-    notes: string,
-    therapistId: number,
-    patientId: number
+export interface GetAppointmentResult {
+  appointmentDate: Date;
+  appointmentId: number;
+  therapistId: number;
+  patientId: number;
+  notes: string;
+  status: "Pending" | "Confirmed" | "Cancelled";
 }
 
+export interface ListAppointmentInput {
+  appointmentDate: Date;
+  appointmentId: number;
+  therapistId: number;
+  patientId: number;
+  notes: string;
+}
+
+export interface ListAppointmentResult {
+  appointmentDate: Date;
+  appointmentId: number;
+  therapistId: number;
+  patientId: number;
+}
+
+export interface UpdateAppointmentInput {
+  appointmentDate: Date;
+  notes: string;
+}
+
+export interface UpdateAppointmentResult {
+  appointmentDate: Date;
+  appointmentId: number;
+  therapistId: number;
+  patientId: number;
+  status: "Pending" | "Confirmed" | "Cancelled";
+}
+
+export interface CreateAppointmentInput {
+  appointmentDate: Date;
+  status: "Pending" | "Confirmed" | "Cancelled";
+}
+
+export interface CreateAppointmentResult {
+  appointmentDate: Date;
+  status: "Pending" | "Confirmed" | "Cancelled";
+}
--- src/entities/health-habit.ts
+++ src/entities/health-habit.ts
@@ -1,31 +1,63 @@
-export interface GetHealthHabitsInput{
-    waterIntake: number,
-    sleepHours: number,
-    mealsEaten: number
-    feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know"
+export interface GetHealthHabitsInput {
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+  feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know";
 }
 
-export interface GetHealthHabitsResults{
-    habitId: number,
-    patientId: number,
-    date: Date,
-    waterIntake: number,
-    sleepHours: number,
-    mealsEaten: number
+export interface GetHealthHabitsResults {
+  habitId: number;
+  patientId: number;
+  date: Date;
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
 }
 
-export interface GetHealthHabitsUpdate{
-    habitId: number,
-    patientId: number,
-    date: Date,
-    waterIntake: number,
-    sleepHours: number,
-    mealsEaten: number
+export interface ListHealthHabitsInput {
+  date: Date;
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+  feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know";
 }
 
-export interface GetHealthHabitsList{
-    waterIntake: number,
-    sleepHours: number,
-    mealsEaten: number
-    feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know"
+export interface ListHealthHabitsResult {
+  date: Date;
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+  feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know";
+}
+
+export interface UpdateHealthHabitsInput {
+  habitId: number;
+  patientId: number;
+  date: Date;
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+}
+
+export interface UpdateHealthHabitsResult {
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+  feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know";
+}
+
+export interface CreateHealthHabitsInput {
+  date: Date;
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+  feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know";
+}
+
+export interface CreateHealthHabitsResult {
+  date: Date;
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+  feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know";
 }
