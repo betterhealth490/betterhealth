de86cb21c08c2a03ee2ea4b2be986831048c2b6e
jesuskcapellan <122330858+jesuskcapellan@users.noreply.github.com>
2024-11-12 20:58:42 -0500

    Create sidebar for user pages (#24)

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,20 +1,13 @@
-import { SignedIn, SignOutButton } from "@clerk/nextjs";
-import { Button } from "~/components/ui/button";
+import { AppSidebar } from "~/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "~/components/ui/sidebar";
 
 export default function UserLayout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <div>
-            {/* TODO: Replace this with the app sidebar */}
-            <SignedIn>
-                <div className="absolute top-0 z-50 flex w-full justify-end gap-2 p-4">
-                    <SignOutButton>
-                        <Button variant="outline">Log out</Button>
-                    </SignOutButton>
-                </div>
-            </SignedIn>
-            {children}
-        </div>
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>{children}</SidebarInset>
+        </SidebarProvider>
     );
 }
