779208f5895983f7188122a8e1b52fb8d4b3c146
Alamdar Qanoongo <72224370+aloomdar@users.noreply.github.com>
2024-11-13 09:49:14 -0500

    Add appointment and health habits entities (#12)
    * Add entities
    
    * add appointment and health habit entities
    
    * Delete src/entities/health-habits.ts because it was pushed twice
    
    * Fix health habit and appointment entities
    
    * add appointment and health habit entities
    
    * Delete src/entities/health-habits.ts because it was pushed twice
    
    * Update list entities
    
    ---------
    
    Co-authored-by: aloomdar <aqanooongo@gmail.com>

--- src/entities/appointment.ts
+++ src/entities/appointment.ts
@@ -1,14 +1,59 @@
-export interface GetAppointmentInput{
-    userId: string,
-    date: Date
+export interface GetAppointmentInput {
+    appointmentId: number;
 }
 
-export interface GetAppointmentResult{
-    appointmentDate: Date,
-    appointmentId: number,
-    status: "Pending" | "Confirmed" | "Cancelled",
-    notes: string,
-    therapistId: number,
-    patientId: number
+export interface GetAppointmentResult {
+  appointmentDate: Date;
+  appointmentId: number;
+  therapistId: number;
+  patientId: number;
+  notes: string;
+  status: "Pending" | "Confirmed" | "Cancelled";
 }
 
+export interface ListAppointmentsInput {
+  userId: number;
+  date: Date;
+}
+
+export interface ListAppointmentsItem {
+  appointmentDate: Date;
+  appointmentId: number;
+  therapistId: number;
+  patientId: number;
+}
+
+export type ListAppointmentsResult = ListAppointmentsItem[];
+
+export interface UpdateAppointmentInput {
+  appointmentDate: Date;
+  appointmentId: number;
+  notes: string;
+  status: "Pending" | "Confirmed" | "Cancelled";
+}
+
+export interface UpdateAppointmentResult {
+  appointmentDate: Date;
+  appointmentId: number;
+  therapistId: number;
+  patientId: number;
+  status: "Pending" | "Confirmed" | "Cancelled";
+}
+
+export interface CreateAppointmentInput {
+    appointmentDate: Date;
+    appointmentId: number;
+    patientId: number;
+    therapistId: number;
+    notes: string;
+    status: "Pending" | "Confirmed" | "Cancelled";
+}
+
+export interface CreateAppointmentResult {
+    appointmentDate: Date;
+    appointmentId: number;
+    patientId: number;
+    therapistId: number;
+    notes: string;
+    status: "Pending" | "Confirmed" | "Cancelled";
+}
---
+++ src/entities/health-habit.ts
@@ -0,0 +1,59 @@
+export interface GetHealthHabitsInput {
+  habitId: number;
+}
+
+export interface GetHealthHabitsResult {
+  habitId: number;
+  patientId: number;
+  date: Date;
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+}
+
+export interface ListHealthHabitsInput {
+  date: Date;
+  userId: number;
+}
+
+export interface ListHealthHabitsItem {
+  date: Date;
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+  feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know";
+} 
+
+export type ListHealthHabitsResult = ListHealthHabitsItem[];
+
+export interface UpdateHealthHabitsInput {
+  habitId: number;
+  patientId: number;
+  date: Date;
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+}
+
+export interface UpdateHealthHabitsResult {
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+  feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know";
+}
+
+export interface CreateHealthHabitsInput {
+  date: Date;
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+  feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know";
+}
+
+export interface CreateHealthHabitsResult {
+  date: Date;
+  waterIntake: number;
+  sleepHours: number;
+  mealsEaten: number;
+  feeling: "Excited" | "Happy" | "Okay" | "Mellow" | "Sad" | "I don't know";
+}
