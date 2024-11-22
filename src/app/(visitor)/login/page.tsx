1edef90b33058805e840514128a8fc26efa04077
Jesus Capellan <jesus.k.capellan@outlook.com>
2024-11-22 15:38:51 -0500

    Complete login with Clerk and colocate visitor components

---
+++ src/app/(visitor)/login/page.tsx
@@ -0,0 +1,9 @@
+import { LogInForm } from "./login-form";
+
+export default function LoginPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <LogInForm />
+    </div>
+  );
+}
