c950605d53e4d20f3e232f54258859eb92b55376
tanvirlongia <102270837+tanvirlongia@users.noreply.github.com>
2024-11-22 13:56:51 -0500

    add list billing entity

--- src/entities/billing.ts
+++ src/entities/billing.ts
@@ -40,3 +40,18 @@ export interface UpdateBillingResult {
     updatedAt: Date| null;
     dueDate: Date;
 }
+
+export interface ListBillingInput {
+    userId: number;
+}
+
+export interface ListBillingItem {
+    billId: number;
+    userId: number;
+    amount: number;
+    dueDate: Date;
+    status: "pending" | "paid"  | null;
+    updatedAt: Date;
+}
+
+export type ListBillingResult = ListBillingItem[];
