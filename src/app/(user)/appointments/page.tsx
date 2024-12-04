51fd8b9e1c1881e6042f8484d82ba9bd900e6976
DavidZamora35 <dz35@njit.edu>
2024-12-04 17:19:52 -0500

    temp

--- src/app/(user)/appointments/page.tsx
+++ src/app/(user)/appointments/page.tsx
@@ -1,5 +1,21 @@
-import { Boilerplate } from "~/components/boilerplate";
+'use client'
+
+import React from "react";
+import { AppointmentCalendar } from "./appointment-calendar";
+import { ScheduleAppointment } from "./schedule-appointment";
+import { isDefined } from "~/lib/utils";
+import { PageWrapper } from "../page-wrapper";
 
 export default function AppointmentsPage() {
-    return <Boilerplate>Appointments</Boilerplate>;
+    const [date, setDate] = React.useState<Date | undefined>(new Date())
+    const today = new Date()
+    
+    return(
+        <PageWrapper className="p-4">
+            <div className="flex justify-center items-center h-full w-full">
+                <AppointmentCalendar date={date} setDate={setDate}/>
+                { isDefined(date) && <ScheduleAppointment date={date}/> }
+            </div>
+        </PageWrapper>
+    )
 }
--- src/app/(user)/page-wrapper.tsx
+++ src/app/(user)/page-wrapper.tsx
@@ -1,20 +1,21 @@
-import React from "react";
+import React, { HTMLAttributes } from "react";
 import { Breadcrumbs } from "~/components/user/breadcrumbs";
+import { cn } from "~/lib/utils";
 
 export function PageWrapper({
   actions,
   children,
+  className,
 }: {
   actions?: React.ReactNode;
-  children: React.ReactNode;
-}) {
+} & HTMLAttributes<HTMLDivElement>) {
   return (
     <div className="flex max-h-screen w-full flex-col">
       <div className="flex items-center justify-between border-b px-4 py-2">
         <Breadcrumbs />
         {actions}
       </div>
-      <div className="flex-grow overflow-auto">{children}</div>
+      <div className={cn("flex-grow overflow-auto", className)}>{children}</div>
     </div>
   );
 }
